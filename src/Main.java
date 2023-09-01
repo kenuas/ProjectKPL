9940d2060c82b2c732290ed0fb68ce1b2c7df5b5
kenuas <kenia.perez@gambridge.edu.mx>
2023-09-01 10:43:07 -0600

    Mensaje de prueba

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        Circulo ciculito=new Circulo();
-        ciculito.setRadio(10);
-        JOptionPane.showMessageDialog(null,"El area es: " +ciculito.calcularArea());
-
+        Tiempo tiempito =new Tiempo();
+        tiempito.setHoras(8);
+        tiempito.setMinutos(23);
+        tiempito.setSegundos(12);
+        JOptionPane.showMessageDialog(null,"El tiempo calculado es: "+tiempito.toString());
 
     }
 }
